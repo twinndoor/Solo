90915a0c25ea43a61cc5f5e0faf64b0816c7f4af
twinndoor <cannonbryant197@gmail.com>
2025-03-22 23:47:56 -0400

    Update models.ts

--- models.ts
+++ models.ts
@@ -1,1 +1,21 @@
+// src/models.ts
 
+export interface Character {
+    name: string;
+    level: number;
+    stats: Stats;
+}
+
+export interface Stats {
+    strength: number;
+    agility: number;
+    intelligence: number;
+    health: number;
+}
+
+export interface DungeonBeast {
+    name: string;
+    level: number;
+    health: number;
+    attack: number;
+}
